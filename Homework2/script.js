b8ce390e73ba71288be16be908634298d54f769d
Orest <orestvaskiv93@gmail.com>
2021-09-27 19:52:04 +0300

    optimization

--- Homework2/script.js
+++ Homework2/script.js
@@ -1,6 +1,6 @@
 let firstNumber = '';
 do {
-    firstNumber = prompt('Введіть перше число:','');
+    firstNumber = prompt('Введіть перше число:','').trim();
 } while (firstNumber === '' || isNaN (firstNumber) || firstNumber === null);
 let parsedFirst = parseInt(firstNumber);
 console.log (parsedFirst);
@@ -8,25 +8,19 @@ console.log (parsedFirst);
 
 let secondNumber = '';
 do {
-    secondNumber = prompt('Введіть друге число:','');
-} while (secondNumber === '' || isNaN(secondNumber) || secondNumber === null);
+    secondNumber = prompt('Введіть друге число:','').trim();
+} while (secondNumber === '' || isNaN (secondNumber) || secondNumber === null);
 let parsedSecond = parseInt(secondNumber);
 console.log (parsedSecond);
 const skipEven = confirm('Чи потрібно пропускати парні числа?');
 
 
 let sum = 0;
-    if (skipEven){
         for (i = parsedFirst; i <= parsedSecond; i ++) {
-            sum += i % 2 ===0 ? 0 : i;
-        }
-    }else {
-        for (i = parsedFirst; i <= parsedSecond; i++) {
-            sum += i;
-        }
+            if (skipEven && i % 2 === 0) {
+                continue;
+            }else {
+                sum += i;    
     }
+}
 alert (`Сума: ${sum}`);
-
-
-
-
