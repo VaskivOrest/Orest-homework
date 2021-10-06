7ce8cffd3cb11b960e61be7314c977a84ab48903
Orest <orestvaskiv93@gmail.com>
2021-10-06 21:47:35 +0300

    fixed HW#4

--- Homework4/script.js
+++ Homework4/script.js
@@ -1,22 +1,58 @@
-const students = ['Олександр', 'Ігор', 'Олена', 'Іра', 'Олексій', 'Світлана'];
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 const themes = ['Диференційне рівняння', 'Теорія автоматів', 'Алгоритм і структури даних'];
 const marks = [4, 5, 5, 3, 4, 5];
 
 
-const getPairs = (students) => [[students[0], students[2]], [students[1], students[3]], [students[4], students[5]]];
-console.log (getPairs(students));
 
-const getStudentsThemes = (students, themes) => [[students[0] + ' i ' + students[2], themes[1]], [students[1] + ' i ' + students[3], themes[0]], [students[4] + ' i ' + students[5], themes[2]]];
-console.log (getStudentsThemes(students, themes));
+const getPairs = (students) => {
+    const boys = [];
+    const girls = [];
+    const pairs = [];
 
-const getMarks = (students, marks) => [[students[0], marks[0]], [students[1],marks[1]], [students[2],marks[2]], [students[3],marks[3]], [students[4],marks[4]], [students[5],marks[5]]];
-console.log (getMarks(students, marks));
+   for (let i = 0;i < students.length; i++) {
+      if (students[i].endsWith("а")){
+        girls.push(students[i])
+    }else {
+        boys.push(students[i])
+    };
+};
+
+    for (let i = 0; i < boys.length; i++) {
+        pairs.push([girls[i], boys[i]]);
+    }
+    return pairs;
+};
+
+
+
+
+const getThemes = (pairs, themes) => {
+    const themesPairs = pairs.map((pair, i) => [pairs[i].join(' і '), themes[i]]);
+    return themesPairs;
+};
+
+
+
+const getStudentMark = (students, marks) => 
+    students.map((item, i) => [students[i], marks[i]]);
+
+
 
 const randomMarks = (min, max) => {
-    return (Math.floor((Math.random() * parseInt(max) ) + parseInt(min)))
+    return (Math.floor((Math.random() * parseInt(5) ) + parseInt(1)))
 }
-const getStudentsThemesMarks = (students, themes) => [[students[0] + ' i ' + students[2], themes[1], randomMarks(1, 5)], [students[1] + ' i ' + students[3], themes[0], randomMarks(1, 5)], [students[4] + ' i ' + students[5], themes[2], randomMarks(1, 5)]];
-console.log (getStudentsThemesMarks(students, themes));
 
+const getStudentsThemesMarks = (pairs, themes) => {
+    const randomMarksPairs = pairs.map((pair, i) => [pairs[i].join(' і '), themes[i], randomMarks()]);
+    return randomMarksPairs;
+};
 
+const pairs = getPairs(students);
+const studentThemes = getThemes(pairs, themes);
+const studentMark = getStudentMark(students, marks);
+const studentsThemesMarks = getStudentsThemesMarks(pairs, themes);
 
+console.log (pairs);
+console.log (studentThemes);
+console.log (studentMark);
+console.log (studentsThemesMarks);
