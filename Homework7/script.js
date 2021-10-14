f4e35d8b9d45c28b55af56d18f3d9a72d53c92fa
Orest <orestvaskiv93@gmail.com>
2021-10-14 22:20:39 +0300

    Done

--- Homework7/script.js
+++ Homework7/script.js
@@ -3,20 +3,42 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 
+
 //1
 const getMyTaxes = function(salary) {
     return this.tax * salary;
 };
-//console.log (getMyTaxes.call(latvia, 1000));
+
+console.log (getMyTaxes.call(latvia, 1000));
+
+
+
 //2
 const getMiddleTaxes = function() {
     return this.tax * this.middleSalary;
 };
-//console.log (getMiddleTaxes.call(litva));
+
+console.log (getMiddleTaxes.call(litva));
+
+
+
 //3
 const getTotalTaxes = function() {
     return this.tax * this.middleSalary * this.vacancies;
 };
-//console.log (getTotalTaxes.call(litva))
-//4
 
+console.log (getTotalTaxes.call(litva))
+
+
+
+//4
+const getMeSalary = function (country) {
+    const salary = Math.random () * (2000 - 1500) + 1500;
+    return {
+        salary: salary.toFixed(1),
+        taxes: (this.tax * salary).toFixed(1),
+        profit: (salary - (this.tax * salary)).toFixed(1),
+    }
+};
+
+setInterval(() => console.log (getMeSalary.call(ukraine)), 10000);
